cac19195a6966dccfb742be4c6081ffc7f31f149
Edivan da Silva Junior <97403013+edivansilvajr@users.noreply.github.com>
2022-10-27 22:27:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let button = document.getElementById('button')
-let cadastro = []
+let valores = []
 
 let tableDados = document.getElementById('dadosTabela')
 let thead = document.createElement('thead')
+let tbody = document.createElement('tbody')
 let tr1 = document.createElement('tr')
 let th1 = document.createElement('th')
 let th2 = document.createElement('th')
@@ -28,34 +29,60 @@ function adicionar() {
         alert('Por favor preencha os campos necessários')
     } else if(isNaN(txtidade.value)) {
         alert('[ERRO campo: Idade] Valor inválido, favor inserir números')
+        txtidade.value = ''
+        txtidade.focus()
     } else if(!isNaN(nome.value)) {
         alert('[ERRO campo: Nome] Valor inválido, favor inserir seu nome corretamente')
     } else if (idade < 18 || idade > 100) {
         alert('Sua idade não se adequa aos pré requisitos')
     
     } else {
-        let tr2 = document.createElement('tr')
-        let td1 = document.createElement('td')
-        let td2 = document.createElement('td')
-        let td3 = document.createElement('td')
-        let btn = document.createElement('button')
         
 
-        let td1texto = document.createTextNode(nome.value)
-        let td2texto = document.createTextNode(idade)
-        let btntexto = document.createTextNode('Excluir')
+        if(valores.indexOf(nome.value.toUpperCase()) != -1) {
+            alert('Esse nome ja foi cadastrado anteriormente')
+            nome.value = ''
+            txtidade.value = ''
+            nome.focus()
+        } else {
+            valores.push(nome.value.toUpperCase())
+            let busca = valores[0]
 
-        thead.appendChild(tr2).appendChild(td1).appendChild(td1texto)
-        thead.appendChild(tr2).appendChild(td2).appendChild(td2texto)
-        thead.appendChild(tr2).appendChild(td3).appendChild(btn).appendChild(btntexto)
+            var tr2 = document.createElement('tr')
+            let td1 = document.createElement('td')
+            let td2 = document.createElement('td')
+            let td3 = document.createElement('td')
+            var btn = document.createElement('button')
+            
 
+            for(var pos = 0; pos < valores.length; pos++) {
+                btn.setAttribute('id', pos)  
+                let btndelete = document.getElementById(pos)
+            }
 
-        cadastro.push(nome.value.toUpperCase())
+            
 
-        
+            let td1texto = document.createTextNode(nome.value)
+            let td2texto = document.createTextNode(idade)
+            let btntexto = document.createTextNode('Excluir')
 
-        
+            thead.appendChild(tr2).appendChild(td1).appendChild(td1texto)
+            thead.appendChild(tr2).appendChild(td2).appendChild(td2texto)
+            thead.appendChild(tr2).appendChild(td3).appendChild(btn).appendChild(btntexto)
+
+            function excluir() {
+                tr2.remove()
+                valores.pop()
+            }
+
+            btn.addEventListener('click', excluir)
+            
+            nome.value = ''
+            txtidade.value = ''
+            nome.focus()
+        } 
     }
+    
     
 }
 
