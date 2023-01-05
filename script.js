c4e5f02e5c1481064bd97ead07a7adf5bbfffa53
EDIVAN DA SILVA JUNIOR <edivanjr13@gmail.com>
2023-01-04 21:25:33 -0300

    FIX Solucionado o problema de excluir nomes do array

--- script.js
+++ script.js
@@ -1,86 +1,83 @@
-let button = document.getElementById('button')
-let valores = []
+const button = document.getElementById('button');
+const valores = [];
 
-let tableDados = document.getElementById('dadosTabela')
-let thead = document.createElement('thead')
-let tbody = document.createElement('tbody')
-let tr1 = document.createElement('tr')
-let th1 = document.createElement('th')
-let th2 = document.createElement('th')
-let th3 = document.createElement('th')
-let th1texto = document.createTextNode('Nome')
-let th2texto = document.createTextNode('Idade')
-let th3texto = document.createTextNode('Ação')
+const tableDados = document.getElementById('dadosTabela');
+const thead = document.createElement('thead');
+const tbody = document.createElement('tbody');
+const tr1 = document.createElement('tr');
+const th1 = document.createElement('th');
+const th2 = document.createElement('th');
+const th3 = document.createElement('th');
+const th1texto = document.createTextNode('Nome');
+const th2texto = document.createTextNode('Idade');
+const th3texto = document.createTextNode('Ação');
 
-tableDados.appendChild(thead).appendChild(tr1).appendChild(th1).appendChild(th1texto)
-tableDados.appendChild(thead).appendChild(tr1).appendChild(th2).appendChild(th2texto)
-tableDados.appendChild(thead).appendChild(tr1).appendChild(th3).appendChild(th3texto)
-
-
-
-
+thead.appendChild(tr1);
+tr1.appendChild(th1);
+th1.appendChild(th1texto);
+tr1.appendChild(th2);
+th2.appendChild(th2texto);
+tr1.appendChild(th3);
+th3.appendChild(th3texto);
+tableDados.appendChild(thead);
 
 function adicionar() {
-    let nome = document.getElementById('nome')
-    let txtidade = document.getElementById('idade')
-    let idade = Number(txtidade.value)
-    
-    if(txtidade.value.length == 0 || nome.value.length == 0) {
-        alert('Por favor preencha os campos necessários')
-    } else if(isNaN(txtidade.value)) {
-        alert('[ERRO campo: Idade] Valor inválido, favor inserir números')
-        
-    } else if(!isNaN(nome.value)) {
-        alert('[ERRO campo: Nome] Valor inválido, favor inserir seu nome corretamente')
+    const nome = document.getElementById('nome');
+    const txtidade = document.getElementById('idade');
+    const idade = Number(txtidade.value);
+
+    if (txtidade.value.length === 0 || nome.value.length === 0) {
+        alert('Por favor preencha os campos necessários');
+    } else if (isNaN(txtidade.value)) {
+        alert('[ERRO campo: Idade] Valor inválido, favor inserir números');
+    } else if (!isNaN(nome.value)) {
+        alert('[ERRO campo: Nome] Valor inválido, favor inserir seu nome corretamente');
     } else if (idade < 18 || idade > 100) {
-        alert('Sua idade não se adequa aos pré requisitos')
-    
+        alert('Sua idade não se adequa aos pré requisitos');
     } else {
-        
-
-        if(valores.indexOf(nome.value.toUpperCase()) != -1) {
-            alert('Esse nome ja foi cadastrado anteriormente')
-
+        if (valores.indexOf(nome.value.toUpperCase()) !== -1) {
+            alert('Esse nome ja foi cadastrado anteriormente');
         } else {
-            valores.push(nome.value.toUpperCase())
-            let busca = valores[0]
+            valores.push(nome.value.toUpperCase());
 
-            var tr2 = document.createElement('tr')
-            let td1 = document.createElement('td')
-            let td2 = document.createElement('td')
-            let td3 = document.createElement('td')
-            var btn = document.createElement('button')
-            
+            const tr2 = document.createElement('tr');
+            const td1 = document.createElement('td');
+            const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
+            const btn = document.createElement('button');
+            btn.setAttribute('id', valores.length - 1);
 
-            for(var pos = 0; pos < valores.length; pos++) {
-                btn.setAttribute('id', pos)  
-                let btndelete = document.getElementById(pos)
-            }
+            const td1texto = document.createTextNode(nome.value);
+            const td2texto = document.createTextNode(idade);
+            const btntexto = document.createTextNode('Excluir');
 
-            
+            tr2.appendChild(td1);
+            td1.appendChild(td1texto);
+            tr2.appendChild(td2);
+            td2.appendChild(td2texto);
+            tr2.appendChild(td3);
+            td3.appendChild(btn);
+            btn.appendChild(btntexto);
+            tbody.appendChild(tr2);
+            tableDados.appendChild(tbody);
 
-            let td1texto = document.createTextNode(nome.value)
-            let td2texto = document.createTextNode(idade)
-            let btntexto = document.createTextNode('Excluir')
+            function excluir(event) {
+                const index = event.target.id;
+                tr2.remove();
+                valores.splice(index, 1);
 
-            tableDados.appendChild(tbody).appendChild(tr2).appendChild(td1).appendChild(td1texto)
-            tableDados.appendChild(tbody).appendChild(tr2).appendChild(td2).appendChild(td2texto)
-            tableDados.appendChild(tbody).appendChild(tr2).appendChild(td3).appendChild(btn).appendChild(btntexto)
-
-            function excluir() {
-                if(valores.length == 1) {
-                    tbody.remove()
+                // Atualiza os atributos 'id' dos botões
+                const buttons = document.querySelectorAll('button');
+                for (let i = 0; i < buttons.length; i++) {
+                    buttons[i].setAttribute('id', i);
                 }
-                tr2.remove()
-                valores.pop()
             }
 
-            btn.addEventListener('click', excluir)
-            
-        } 
+
+            btn.addEventListener('click', excluir);
+        }
     }
-    
-    
 }
 
-button.addEventListener('click', adicionar)
+button.addEventListener('click', adicionar);
+
